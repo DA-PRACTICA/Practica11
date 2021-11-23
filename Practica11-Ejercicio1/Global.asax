f3253e0e7f83e428270adb86c67c145718f69951
Jeremy Huaringa <jeremy.huaringa@ucsm.edu.pe>
2021-11-23 13:31:28 -0500

    Ejercicio 1

---
+++ Practica11-Ejercicio1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Practica11_Ejercicio1.MvcApplication" Language="C#" %>
---
+++ Practica11-Ejercicio1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Practica11_Ejercicio1.Startup))]
+namespace Practica11_Ejercicio1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
